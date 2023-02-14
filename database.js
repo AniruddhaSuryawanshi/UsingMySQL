d1422e2c6e280cd88939d3e902421f1802d94eef
スルヤワン　アニルド <117163734+AniruddhaSuryawanshi@users.noreply.github.com>
2023-02-14 16:57:22 +0900

    Add files via upload

---
+++ database.js
@@ -0,0 +1,42 @@
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+export async function getNotes() {
+  const [rows] = await pool.query("SELECT * FROM notes");
+  return rows;
+}
+
+export async function getNote(id) {
+  const [rows] = await pool.query(
+    `
+    SELECT *
+    FROM notes 
+    WHERE id=?
+    `,
+    [id]
+  );
+  return rows[0];
+}
+
+export async function createNote(title, content) {
+  const [result] = await pool.query(
+    `
+    INSERT INTO notes (title, contents)
+    VALUES (?, ?)
+    `,
+    [title, content]
+  );
+  const id = result.insertId;
+  return getNote(id);
+}
